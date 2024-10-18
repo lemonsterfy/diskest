e1b22c936376df4b17fb428fbfc5c1aac94e2fa4
lemonster <li.meng.shanghai@gmail.com>
2024-10-18 17:04:34 +0800

    commit

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
 from setuptools import setup
 
-setup()
+
+def read_requirements(filename):
+    with open(filename) as f:
+        return [line.strip() for line in f if line.strip() and not line.startswith("#")]
+
+
+setup(
+    extras_require={
+        "dev": read_requirements("requirements-dev.txt"),
+    },
+)
