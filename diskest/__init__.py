af1ad235cf24349240228b13b0dc51cf176bed09
lemonster <li.meng.shanghai@gmail.com>
2024-10-18 17:19:01 +0800

    commit

--- diskest/__init__.py
+++ diskest/__init__.py
@@ -5,7 +5,11 @@ This package provides a comprehensive set of tools for benchmarking
 disk performance on Linux systems.
 """
 
-__version__ = "0.1.0"
 __author__ = "lemonsterfy"
 __license__ = "MIT"
 __email__ = "lemonsterfy@gmail.com"
+
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ = "unknown"
